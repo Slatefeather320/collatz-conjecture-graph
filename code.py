32925f5e56a800fb8eaf87b7d971f95a7bae424b
Slate <52231034+Slatefeather320@users.noreply.github.com>
2022-09-27 16:27:00 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,15 +1,16 @@
-def cool(x):
+
+def cool(x, e, o): 
     result = []
     cooling = True
     while cooling:
         if x%2==0:
-            x /= 2 
+            x /= e 
         else:
-            x = (x*3)+1
+            x = (x*o)+1
         
         if x in result: #breaks the loop when numbers start repeating
             cooling = False
-        result.append(int(x)) 
+        result.append(x) 
     return result
 
-print(cool(50))
+print(cool(50,2,3))
